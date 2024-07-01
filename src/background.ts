d36a94ffff44a85d42055561f2ea644809901c7d
Federico Brigante <me@fregante.com>
2024-07-01 08:33:22 +0000

    Update `webext-permission-toggle`

--- src/background.ts
+++ src/background.ts
@@ -1,7 +1,7 @@
-import addDomainPermissionToggle from 'webext-domain-permission-toggle'
+import addPermissionToggle from 'webext-permission-toggle'
 import 'webext-dynamic-content-scripts'
 
-addDomainPermissionToggle({
+addPermissionToggle({
   title: 'Enable Gitako on this domain',
   reloadOnSuccess: 'Refresh to activate Gitako?',
 })
